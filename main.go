3969af3f946692bbcdfc219558419d3b3dc05dfb
Kenji Kaneda <kenji.kaneda@gmail.com>
2016-01-21 19:19:55 -0800

    Add ImportPaths

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"os"
 
 	"github.com/kkaneda/returncheck/internal"
+	"github.com/kisielk/gotool"
 )
 
 func main() {
 	// TODO(kaneda): Add a commandline flag for specifying a target type.
 	targetPkg := "github.com/cockroachdb/cockroach/roachpb"
 	targetTypeName := "Error"
-	if err := returncheck.Run(os.Args[1:], targetPkg, targetTypeName); err != nil {
+	if err := returncheck.Run(gotool.ImportPaths(os.Args[1:]), targetPkg, targetTypeName); err != nil {
 		os.Exit(1)
 	}
 }
